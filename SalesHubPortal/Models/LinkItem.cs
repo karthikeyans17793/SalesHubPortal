b66b01a3fbf6d103f90023b9bcf54ff186f9f529
Karthikeyan (CONTRACTOR) S <karthikeyan.s@iaai.com>
2026-02-16 23:21:21 +0530

    Add project files.

---
+++ SalesHubPortal/Models/LinkItem.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesHubPortal.Models;
+
+public class LinkItem
+{
+    public string Title { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
---
+++ SalesHubPortal/Models/Section.cs
@@ -0,0 +1,12 @@
+﻿using SalesHubPortal.Models;
+
+namespace SalesHubPortal.Models
+{
+
+    public class Section
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<LinkItem> Links { get; set; } = new();
+    }
+
+}
